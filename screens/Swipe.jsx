8d7d1bfe1204a8fdb9e5e1a6a71adb215c7636dc
Berend van Gaart <berendvangaart@gmail.com>
2022-12-02 14:48:42 +0100

    implement swipe controls and butttons

--- screens/Swipe.jsx
+++ screens/Swipe.jsx
@@ -1,10 +1,10 @@
-import React, {useState} from 'react';
+import React, {useMemo, useState} from 'react';
 import {Image, Pressable, Text, View} from "react-native";
 import TinderCard from 'react-tinder-card';
 import Card from "../Components/card/Card";
 import {defaultStyles} from "../styles";
 
-const mockData = [
+const db = [
     {
         name: 'Richard Hendricks',
         img: require('../assets/img/richard.jpg'),
@@ -32,17 +32,35 @@ const mockData = [
     }
 ]
 
+const alreadyRemoved = []
+let charactersState = db;
+
 const Swipe = ({navigation}) => {
-    const people = mockData
+    const [characters, setCharacters] = useState(db)
     const [lastDirection, setLastDirection] = useState()
+    const childRefs = useMemo(() => Array(db.length).fill(0).map(i => React.createRef()), [])
 
     const swiped = (direction, nameToDelete) => {
-        console.log('removing: ' + nameToDelete)
+        console.log('removing: ' + nameToDelete + ' to the ' + direction)
         setLastDirection(direction)
+        alreadyRemoved.push(nameToDelete)
     }
 
     const outOfFrame = (name) => {
         console.log(name + ' left the screen!')
+        charactersState = charactersState.filter(character => character.name !== name)
+        setCharacters(charactersState)
+    }
+
+
+    const swipe = (dir) => {
+        const cardsLeft = characters.filter(person => !alreadyRemoved.includes(person.name))
+        if (cardsLeft.length) {
+            const toBeRemoved = cardsLeft[cardsLeft.length - 1].name // Find the card object to be removed
+            const index = db.map(person => person.name).indexOf(toBeRemoved) // Find the index of which to make the reference to
+            alreadyRemoved.push(toBeRemoved) // Make sure the next card gets removed next time if this card do not have time to exit the screen
+            childRefs[index].current.swipe(dir) // Swipe the card!
+        }
     }
 
     return (
@@ -50,24 +68,37 @@ const Swipe = ({navigation}) => {
 
             <View style={styles.header}>
                 <Text style={styles.title}>Discover</Text>
-                <Pressable style={styles.IconContainer} onPress={() => navigation.navigate('Home')} >
+                <Pressable style={styles.IconContainer} onPress={() => navigation.navigate('Home')}>
                     <Image style={styles.icon} source={require('../assets/matchIcon.png')} alt=""/>
                 </Pressable>
+            </View>
+
+            <View style={styles.container}>
+                <View style={styles.cardContainer}>
+                    {characters.map((character, index) =>
+
+                        <TinderCard ref={childRefs[index]} key={character.name} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)}>
+                            <Card character={character}/>
+                        </TinderCard>
+                    )}
+                </View>
+                {lastDirection ? <Text style={styles.infoText}>You swiped {lastDirection}</Text> :
+                    <Text style={styles.infoText}/>}
+            </View>
+
+            <View style={styles.footer}>
+                <Pressable style={styles.button}  onPress={() => swipe('left')}>
+                    <Image style={styles.btnIcon} source={require('../assets/dislike.png')} alt=""/>
+                </Pressable>
+                <Pressable style={styles.button}  onPress={() => swipe('right')}>
+                    <Image style={styles.btnIcon} source={require('../assets/like.png')} alt=""/>
+                </Pressable>
+
+
 
             </View>
-        <View style={styles.container}>
-            <View style={styles.cardContainer}>
-                {people.map((character, index) =>
 
-                    <TinderCard key={index} onSwipe={(dir) => swiped(dir, character.name)}
-                                onCardLeftScreen={() => outOfFrame(character.name)}>
-                        <Card character={character}/>
-                    </TinderCard>
-                )}
-            </View>
-            {lastDirection ? <Text style={styles.infoText}>You swiped {lastDirection}</Text> :
-                <Text style={styles.infoText}/>}
-        </View>
+
         </>
     )
 };
@@ -86,7 +117,6 @@ const styles = {
     IconContainer: {
         height: 54,
         width: 54,
-
         marginRight: 32,
         alignItems: 'center',
         justifyContent: 'center',
@@ -94,11 +124,9 @@ const styles = {
         borderRadius: 15,
         borderColor: '#999999',
     },
-
     icon : {
         height: 22,
         width: 22,
-
     },
     container: {
         display: 'flex',
@@ -107,15 +135,39 @@ const styles = {
         width: '100%',
     },
     cardContainer: {
-        margin: 100,
+        margin: 50,
         width: '90%',
         maxWidth: 320,
-        height: 300,
+        height: 400,
     },
     infoText: {
         height: 28,
         justifyContent: 'center',
         display: 'flex',
         zIndex: -100,
+    },
+    footer: {
+        width:' 100%',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        // backgroundColor: '#e5c2c2'
+
+    },
+    button: {
+        margin: 5,
+        height: 80,
+        backgroundColor: 'rgba(255,255,255,0.61)',
+        width: 80,
+        alignItems: 'center',
+        justifyContent: 'center',
+        // borderWidth: 1,
+        borderRadius: 100,
+        // borderColor: '#999999',
+
+
+    }, btnIcon : {
+        height: 30,
+        width: 30,
     }
 }
