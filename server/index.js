95c7b55610543bf7a4f71743efdb854b9c45e993
Berend van Gaart <berendvangaart@gmail.com>
2022-12-04 12:51:20 +0100

    back end: create new user

--- server/index.js
+++ server/index.js
@@ -3,15 +3,65 @@ const PORT = 8080
 const {MongoClient} = require('mongodb')
 require('dotenv').config()
 const URI = `mongodb+srv://berend:${process.env.MONGO_KEY}@cluster0.6cnzvon.mongodb.net/?retryWrites=true&w=majority`
+const { v4: uuidv4 } = require( 'uuid')
+const bcrypt = require("bcrypt");
+const cors = require('cors')
+
 
 const app = express()
+
+app.use(cors())
+app.use(express.json())
+
 
 app.get('/', (req, res) => {
     res.json('hello server')
 })
 
-app.post('/signup', (req, res) => {
-    res.json('hello server')
+/**
+ * create a new user
+ * hash passwords create unique id - persist in db
+ */
+app.post('/signup', async (req, res) => {
+    const client = new MongoClient(URI)
+
+    console.log(req.body)
+
+    const userRequestBody = await req.body
+    const hashedPassword = await bcrypt.hash(userRequestBody.password, 10)
+
+    const user = {
+        id: uuidv4(),
+        img: userRequestBody.img,
+        firstName: userRequestBody.firstName,
+        lastName: userRequestBody.lastName,
+        bio: userRequestBody.bio,
+        password: await hashedPassword,
+        email: userRequestBody.email.toLowerCase(),
+        linkedin: userRequestBody.linkedin,
+        jobTitle: userRequestBody.jobTitle,
+        matches: []
+    }
+
+    try {
+
+        await client.connect()
+        const db = client.db('app')
+        const users = db.collection('users')
+
+        const exists = users.findOne({email : userRequestBody.email.toLowerCase()})
+        console.log(exists)
+
+        // if (exists) return res.status(409).send('user already exists.')
+
+        const insertedUser = await users.insertOne(user)
+
+        console.log(insertedUser)
+
+    } finally {
+        await client.close()
+    }
+
 })
 
 app.get('/users', async (req, res) => {
@@ -24,10 +74,9 @@ app.get('/users', async (req, res) => {
         const returnedUsers = await users.find().toArray()
         res.send(returnedUsers)
     } catch (e) {
-        console.log(e)
-        console.error('error fetch users')
+        console.error('error fetch users', e)
     } finally {
-        client.close()
+        await client.close()
     }
 })
 
